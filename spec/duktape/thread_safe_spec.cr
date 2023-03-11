98a8999479a0c09e9de159ad80abc25fb77a9670
chendo <chendo@users.noreply.github.com>
2023-03-11 15:06:06 +1100

    We can't check the ctx directly in the Fiber because there's no mutex around it, at least not yet

--- spec/duktape/thread_safe_spec.cr
+++ spec/duktape/thread_safe_spec.cr
@@ -9,7 +9,7 @@ describe Duktape::Runtime do
   context "thread-safety" do
     describe "push_global_proc" do
       it "should not crash when called from multiple threads" do
-        ctx.push_global_proc("add_one", 1) do |ptr|
+        ctx.push_global_proc("sleep", 1) do |ptr|
           env = Duktape::Context.new ptr
   
           begin
@@ -18,19 +18,17 @@ describe Duktape::Runtime do
             next env.call_failure :type
           end
   
-          env << num + 1
+          print "."
+          sleep(num)
           env.call_success
         end
   
         100.times do
           spawn do
-            rc = ctx.eval!("add_one(42);")
-  
-            rc.should eq(0)
-            ctx.is_error(-1).should be_false
-            ctx.get_number(-1).should eq(43)
+            rc = ctx.eval!("sleep(0.01);")
           end
         end
+
         sleep(1)
       end
     end
